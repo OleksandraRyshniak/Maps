a006b9c114340849bbbc8febf7a529eb62eee25e
OleksandraRyshniak <oleksandraryshniak@gmail.com>
2026-09-20 19:05:19 +0300

    Add satellite layer and control options to map

--- leafletJS/leafletJS.js
+++ leafletJS/leafletJS.js
@@ -1,12 +1,22 @@
 const map = L.map('map').setView([59.42, 24.79], 12);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors',
-    maxZoom: 19
+const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-omnivore.kml('../data/map.kml')
+const satelliit = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri'
+});
+
+const kml = omnivore.kml('../data/map.kml')
     .on('ready', function () {
         map.fitBounds(this.getBounds());
     })
     .addTo(map);
+
+L.control.layers(
+    { 'Kaart': osm, 'Satelliit': satelliit },
+    { 'KML': kml }
+).addTo(map);
+
+L.control.scale().addTo(map);
